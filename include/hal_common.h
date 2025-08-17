a68b84cd97ac59a2f4eaceb48ad7447153860272
Fo-Zi <facundozingarelli@gmail.com>
2025-08-17 14:14:01 -0300

    Adding delay defs

--- include/hal_common.h
+++ include/hal_common.h
@@ -5,4 +5,18 @@
 
 typedef uint16_t hal_timeout_ms;
 
+/**
+ * @brief Delay for specified number of microseconds
+ *
+ * @param microseconds Number of microseconds to delay
+ */
+void hal_delay_microseconds(uint32_t microseconds);
+
+/**
+ * @brief Delay for specified number of milliseconds
+ *
+ * @param milliseconds Number of milliseconds to delay
+ */
+void hal_delay_milliseconds(uint32_t milliseconds);
+
 #endif
